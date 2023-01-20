dffc6a914890cb73c66ecbe6cdc38ce6c8f818ac
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-01-20 12:45:21 -0500

    Classified HDL results in qualitiative terms

--- 01_20/blood_calculator.py
+++ 01_20/blood_calculator.py
@@ -14,5 +14,15 @@ def HDL_input():
     HDL_value = int(input("Enter the HDL result: "))
     return HDL_value
 
+def HDL_analysis(HDL_int):
+    if HDL_int >= 60:
+        answer = "Normal"
+    elif 40 <= HDL_int < 60:
+        answer = "Borderline low"
+    else:
+        answer = "Low"
+        
+    return answer
+
 
 interface()
