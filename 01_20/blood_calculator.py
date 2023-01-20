29d5098522c89731b6c94161c2bb1bca42749048
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-01-20 12:49:04 -0500

    Connected HDL_driver function to the interface

--- 01_20/blood_calculator.py
+++ 01_20/blood_calculator.py
@@ -2,13 +2,21 @@ def interface():
     print("Blood Calculator")
     while True:
         print("Select an option:")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Select an option: ")
         
         if choice == "9":
             return
+        elif choice == "1":
+            HDL_driver()
 
     print("Program ended")
+
+def HDL_driver():
+    value = HDL_input()
+    result = HDL_analysis(value)
+    print(result)
 
 def HDL_input():
     HDL_value = int(input("Enter the HDL result: "))
