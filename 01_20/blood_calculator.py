34d01e0b2fc4259c0539941ec3c29e5b27c0e6c2
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-01-20 12:31:54 -0500

    Developed basic interface with exit option

---
+++ 01_20/blood_calculator.py
@@ -0,0 +1,13 @@
+def interface():
+    print("Blood Calculator")
+    while True:
+        print("Select an option:")
+        print("9 - Quit")
+        choice = input("Select an option: ")
+        
+        if choice == "9":
+            return
+
+    print("Program ended")
+
+interface()
