501a0751db2e38258a38515e46325b1832616245
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-02-01 13:00:53 -0500

    Added README file and added room number functionality to database.py

--- 02_01/database.py
+++ 02_01/database.py
@@ -1,5 +1,5 @@
 def create_patient_entry(patient_name,patient_mrn,patient_age):
-    new_patient = [patient_name,patient_mrn,patient_age]
+    new_patient = [patient_name,patient_mrn,patient_age, []]
     return new_patient
     
 def main_driver():
@@ -10,21 +10,35 @@ def main_driver():
     db.append(create_patient_entry("Bob Saget",3,11))
     
     print(db)
-    print("Get Patient Ann")
-    found_patient = get_patient_entry(db,4)
+    add_test_to_patient(db,4,"HDL",120)
+    room_numbers = ["103","232","333"]
+    print_directory(db,room_numbers)
     
-    if found_patient is False:
-        print("Patient mrn {} not found".format(found_patient))
-    else:
-        print(found_patient)
+def print_directory(db,room_numbers):
+    for i, patient in enumerate(db):
+        print("Patient {} is in room {}".format(patient[0],room_numbers[i]))
         
+        # OR
+        
+    for patient,room_num in zip(db,room_numbers):
+        print("Patient {} is in room {}".format(patient[0],room_num))
+        
+    
 def get_patient_entry(db,mrn_to_find):
     for patient in db:
         if patient[1] == mrn_to_find:
             return patient
     return False
     
-
-
+def add_test_to_patient(db,mrn_to_find, test_name, test_value):
+    patient = get_patient_entry(db,mrn_to_find)
+    
+    if patient is False:
+        print("Bad entry")
+    else:
+        patient[3].append([test_name,test_value])
+    return
+    
+    
 if __name__ == '__main__':
     main_driver()
