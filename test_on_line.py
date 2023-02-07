84fa12a6ee0e312092c7839b0bc235a76e470ac1
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-02-07 10:34:06 -0500

    Fixed typo on line 23 that contained extra parameter and tested the test function...all passed

--- test_on_line.py
+++ test_on_line.py
@@ -16,11 +16,11 @@ def test_on_line(point_1, point_2, x, expected):
     
     
 @pytest.mark.parametrize("point_1, point_2, expected",
-                         [((0, 0), (1, 1), 1)
+                         [((0, 0), (1, 1), 1),
                           ((0, 0), (2, 1), 0.5),
                              ((0, 0), (1, 2), 2)])
                              
-def test_get_slope(point_1, point_2, x, expected):
+def test_get_slope(point_1, point_2, expected):
 
     from on_line import get_slope
 
