0dfdb5a119be579259f6924f5ae682803d0f3036
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-02-10 12:38:25 -0500

    Converted add_test_to_patient and get_test_result to function with dictionary

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ def create_patient_entry(first_name, last_name, patient_mrn, patient_age):
     new_patient = {'First Name': first_name,
                     'Last Name': last_name,
                     'MRN': patient_mrn,
-                    'Age': patient_age}
+                    'Age': patient_age,
+                    'Tests': []}
     return new_patient
 
 
@@ -14,11 +15,13 @@ def main_driver():
     db.append(create_patient_entry("Bob", "Saget", 3, 11))
 
 
-    print(db)
-    return
     add_test_to_patient(db, 3, "HDL", 120)
     add_test_to_patient(db, 3, "LDL", 68)
 
+    print_database(db)
+    print(db)
+    return
+    
     room_numbers = ["103", "232", "333"]
     print_directory(db, room_numbers)
 
@@ -41,7 +44,7 @@ def find_result(db, mrn, test_name):
     if patient is False:
         print("MRN #{} not found".format(mrn))
 
-    for test_info in patient[3]:
+    for test_info in patient["Tests"]:
         if test_info[0] == test_name:
             return test_info[1]
 
@@ -50,7 +53,7 @@ def find_result(db, mrn, test_name):
 
 def get_patient_entry(db, mrn_to_find):
     for patient in db:
-        if patient[1] == mrn_to_find:
+        if patient['MRN'] == mrn_to_find:
             return patient
     return False
 
@@ -61,9 +64,18 @@ def add_test_to_patient(db, mrn_to_find, test_name, test_value):
     if patient is False:
         print("Bad entry")
     else:
-        patient[3].append([test_name, test_value])
+        patient["Tests"].append([test_name, test_value])
     return
 
+
+def get_full_name(patient_dict):
+    full_name = patient_dict["First Name"] + ' ' + patient_dict["Last Name"]
+    return full_name
+    
+def print_database(patient_dict):
+    for patient in patient_dict:
+        print("MRN: {}, Full Name: {}, Age: {}".format(patient["MRN"], get_full_name(patient), patient["Age"]))
+        
 
 if __name__ == '__main__':
     main_driver()
