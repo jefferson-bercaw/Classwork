aded9d36a4d61ac1f7026ddb8081047c1ce04787
Jefferson Bercaw <jefferson.bercaw@duke.edu>
2023-02-15 12:02:10 -0500

    Changed database functionality to a class

---
+++ database_class.py
@@ -0,0 +1,32 @@
+class Patient:
+
+    def __init__(self, patient_first_name,
+                 patient_last_name, patient_mrn, patient_age):
+        self.first_name = patient_first_name
+        self.last_name = patient_last_name
+        self.mrn = patient_mrn
+        self.age = patient_age
+        self.tests = []
+
+    def get_full_name(self):
+        full_name = self.first_name + ' ' + self.last_name
+        return full_name
+        
+    def is_adult(self):
+        if self.age >= 18:
+            return True
+        return False
+
+
+def main():
+
+    new_patient = Patient("David", "Ward", 1, 56)
+    print(new_patient)
+
+    print(new_patient.first_name)
+    print(new_patient.last_name)
+    print(new_patient.get_full_name())
+    print(new_patient.is_adult())
+
+if __name__ == '__main__':
+    main()
